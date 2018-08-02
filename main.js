8519dd1838a1710ca561f6fc736f8fe8b222979a
clinturbin <clinturbin@gmail.com>
2018-08-02 08:41:53 -0400

    Made site that justs turns a square 45 degrees when button is clicked.

---
+++ main.js
@@ -0,0 +1,27 @@
+var box = document.querySelector('.box');
+var turnButton  = document.querySelector('.turn-button');
+
+
+turnButton.addEventListener('click', function() {
+    if (box.classList.contains("rotate0")) {
+        box.classList.remove("rotate0");
+        box.classList.add("rotate45");
+        console.log('rotate 45');
+    }
+    else if (box.classList.contains("rotate45")) {
+        box.classList.remove("rotate45");
+        box.classList.add("rotate90");
+        console.log('rotate 90');
+    }
+    else if (box.classList.contains("rotate90")) {
+        box.classList.remove("rotate90");
+        box.classList.add("rotate135");
+        console.log('rotate 135');
+    }
+    else if (box.classList.contains("rotate135")) {
+        box.classList.remove("rotate135");
+        box.classList.add("rotate0");
+        console.log('rotate 0');
+    }
+
+});
